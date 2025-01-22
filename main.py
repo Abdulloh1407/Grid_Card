4e65f7b56e7c13c135e0290c2d64badeef89067f
Abdulloh1407 <asultonkabirov@gmail.com>
2025-01-22 09:50:37 +0500

    f

--- main.py
+++ main.py
@@ -1,11 +1,62 @@
-lst=[True,[False,[True,[False,[True]]]]]
 
-def inlist(a):
-    for i in a:
-        if type(i)==set or type(i)==list or type(i)==tuple: 
-            inlist(i)
-        else:
-            if i==True:
-                print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from person import Person/
+# from person import Rectangle
+
+# lst=[True,[False,[True,[False,[True]]]]]
+
+# def inlist(a):
+#     for i in a:
+#         if type(i)==set or type(i)==list or type(i)==tuple: 
+#             inlist(i)
+#         else:
+#             if i==True:
+#                 print(i)
           
-inlist(lst)
+# inlist(lst)
+
+# p1=User("Anna")
+# p2=User("John")
+# p3=User("John")
+# p1.party()
+# p2.party()
+# p3.party()
+
+
+
+
+# p1=Rectangle(10,20)
+# p2=Rectangle(20,30)
+
+# p1.area()
+# p2.area()
+
+
+
---
+++ person.py
@@ -0,0 +1,23 @@
+# class Person:
+#     def __init__(self,name,age,salary):
+#         self.name=name
+#         self.age=age
+#         self.salary=salary
+
+
+# class User:
+#     def __init__(self,name,age,salar):
+#         self.name=name
+       
+#     def party(self):
+#             print( f"Happy new year {self.name}")
+
+
+
+
+# class Rectangle:
+#     def __init__(self,a,b):
+#             self.a=a
+#             self.b=b
+#     def area(self):
+#         print(f"Yuzi:{self.a*self.b}\n Piremetir:{2*(self.a+self.b)}")
